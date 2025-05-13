42d5ec4a31debeb6be6cb79eb2f0c42cec5ac203
OSAMA KHAN <109853647+Osama066@users.noreply.github.com>
2025-05-13 20:53:14 +0530

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,55 @@
+                                                                     --find top 10 highest reveue generating products 
+select * from df_orders
+
+select top 10  product_id,sum (sale_price) as Sales
+from df_orders
+group by product_id 
+order by Sales desc
+
+
+                                                                      --find top 5 highest selling product in each region
+--select * from df_orders
+with cte as (
+select region,product_id,sum(sale_price) as sales
+from df_orders
+group by region,product_id)
+select * from (
+select *
+, row_number() over(partition by region order by sales desc) as rn
+from cte) A
+where rn<=5
+
+                                                                              ----find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
+WITH cte AS (
+    SELECT 
+        YEAR(order_date) AS order_year,
+        MONTH(order_date) AS order_month,
+        SUM(sale_price) AS sales
+    FROM df_orders
+    GROUP BY YEAR(order_date), MONTH(order_date)
+)
+
+SELECT 
+    order_month,
+    SUM(CASE WHEN order_year = 2022 THEN sales ELSE 0 END) AS sales_2022,
+    SUM(CASE WHEN order_year = 2023 THEN sales ELSE 0 END) AS sales_2023
+FROM cte 
+GROUP BY order_month
+ORDER BY order_month;
+
+                                                                                   ----for each category which month had highest sales 
+
+with cte as (
+select category,format(order_date,'yyyyMM') as order_year_month
+, sum(sale_price) as sales 
+from df_orders
+group by category,format(order_date,'yyyyMM')
+--order by category,format(order_date,'yyyyMM')
+)
+select * from (
+select *,
+row_number() over(partition by category order by sales desc) as rn
+from cte
+) a
+where rn=1
+
